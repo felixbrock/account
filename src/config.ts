67288f2cf313d3ddae8bb5007b0d10f6e918459a
Felix Brockmeier <felix.brockmeier@outlook.com>
2021-09-29 17:25:21 +0200

    dev/test env

--- src/config.ts
+++ src/config.ts
@@ -2,13 +2,36 @@ export const nodeEnv = process.env.NODE_ENV || 'development';
 export const defaultPort = 8081;
 export const port = process.env.PORT ? parseInt(process.env.PORT, 10) : defaultPort;
 export const apiRoot = process.env.API_ROOT || "api";
+
+const getMongodbConfig = (): any => {
+  const mongodb: any = {};
+
+  switch (nodeEnv) {
+    case 'development':
+      mongodb.url = process.env.DATABASE_DEV_URL || '';
+      mongodb.dbName = process.env.DATABASE_DEV_NAME || '';
+      break;
+    case 'test':
+      mongodb.url = process.env.DATABASE_TEST_URL || '';
+      mongodb.dbName = process.env.DATABASE_TEST_NAME || '';
+      break;
+    case 'production':
+      mongodb.url = process.env.DATABASE_TEST_URL || '';
+      mongodb.dbName = process.env.DATABASE_TEST_NAME || '';
+      break;
+    default:
+      break;
+  }
+
+  return mongodb;
+};
+
 export const appConfig = {
   express: {
     port,
-    mode: nodeEnv
+    mode: nodeEnv,
   },
   mongodb: {
-    url: process.env.DATABASE_URL || '',
-    dbName: process.env.DATABASE_NAME || ''
-  }
+    ...getMongodbConfig(),
+  },
 };
